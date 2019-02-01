66b6f3efd66632cbd332f2eb8772502398c2b4a5
magael <mikael.jaakkola@helsinki.fi>
2019-02-01 21:33:09 +0200

    2.8-9

--- osa2/osa2-b/src/App.js
+++ osa2/osa2-b/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 // import Name from ".components/Name";
 
-const Persons = (props) => {
-  console.log(props)
-  const { person } = props;
-  return <p>{person.name}</p>;
+const Persons = ({ person }) => {
+  return (
+    <p>
+      {person.name} {person.number}
+    </p>
+  );
 };
 
 const App = () => {
@@ -15,47 +17,71 @@ const App = () => {
     { name: "Lea Kutvonen", number: "040-123456" }
   ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
   const handleNameChange = event => {
     console.log(event.target.value);
     setNewName(event.target.value);
   };
 
-  const addName = event => {
+  const handleNumberChange = event => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = event => {
+    console.log(event.target.value);
+    setNewFilter(event.target.value);
+  };
+
+  const addPerson = event => {
     event.preventDefault();
 
     const names = persons.map(person => person.name);
-
-    if (names.includes(newName)) {
-      console.log("on jo")
-      return (window.alert(`${newName} on jo luettelossa`));
-    }
-    console.log(persons)
-    console.log("ei ollu??")
+    if (names.includes(newName))
+      return window.alert(`${newName} on jo luettelossa`);
 
     const nameObject = {
       name: newName,
-      number: "0",
-    }
+      number: newNumber
+    };
 
     setPersons(persons.concat(nameObject));
     console.log("nappia painettu", event.target);
   };
 
-  const rows = () =>
-    persons.map(person => <Persons key={person.name} person={person} />);
+  const rows = () => {
+    const filteredPersons = persons.filter(person =>
+      person.name.toLowerCase().includes(newFilter.toLowerCase())
+    );
+    console.log(filteredPersons);
+    return filteredPersons.map(person => <Persons key={person.name} person={person} />);
+  };
 
   return (
     <div>
-      <h2>Puhelinluettelo</h2>
-      <form onSubmit={addName}>
+      <h1>Puhelinluettelo</h1>
+      <form>
+        <div>
+          rajaa näytettäviä{" "}
+          <input value={newFilter} onChange={handleFilterChange} />
+        </div>
+      </form>
+
+      <h2>lisää uusi</h2>
+      <form onSubmit={addPerson}>
         <div>
           nimi: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          numero : <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">lisää</button>
         </div>
       </form>
+
       <h2>Numerot</h2>
       <div>{rows()}</div>
     </div>
