6bb1e9caa530ed16d8afcbe4071c4f253bac7dd3
magael <mikael.jaakkola@helsinki.fi>
2019-02-16 15:21:22 +0200

    4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,18 @@
 const dummy = blogs => {
   if (blogs) return 1
+  return 1
+}
+
+const totalLikes = blogs => {
+  const likes = blogs.map(blog => blog.likes)
+  if (likes < 1) return 0
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+  return likes.reduce(reducer, 0)
 }
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
