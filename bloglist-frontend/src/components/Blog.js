76638da05630804ebb4bde281ae1503c3dc58c74
magael <mikael.jaakkola@helsinki.fi>
2019-02-23 18:30:30 +0200

    5.6

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  const details = { display: showDetails ? "" : "none" };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => setShowDetails(!showDetails)}>
+        {blog.title} {blog.author}
+        <div style={details}>
+          {blog.url}
+          <br />
+          {blog.likes} likes <button>like</button>
+          <br />
+          added by {blog.user.name}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
