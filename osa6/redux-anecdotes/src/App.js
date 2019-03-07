fcaeec7e075c4d980f50e43863c7fc207cf58364
Mikael Jaakkola <mikael.jaakkola@helsinki.fi>
2019-03-07 23:14:03 +0200

    6.11

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import AnecdoteList from "./components/AnecdoteList";
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
+import Filter from "./components/Filter";
 
 const App = props => (
   <div>
     <h2>Anecdotes</h2>
     <Notification store={props.store} />
+    <Filter store={props.store} />
     <AnecdoteList store={props.store} />
     <h2>create new</h2>
     <AnecdoteForm store={props.store} />
--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,15 +2,26 @@ import React from "react";
 import { vote } from "../reducers/anecdoteReducer";
 import { clearMessage } from "../reducers/notificationReducer";
 
-const AnecdoteList = props => {
-  const store = props.store;
+const AnecdoteList = ({ store }) => {
+  const { anecdotes, notification, filter } = store.getState();
 
+  const anecdotesToShow = () => {
+    if (filter === "ALL") {
+      return anecdotes;
+    }
+    return anecdotes.filter(a =>
+      a.content.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  // Tän vois tietty eriyttää omaksi komponentiksi
   const upvote = (id, content) => {
-    // voisi säätää enemmänkin, että joka viesti olisi aina sen 5 sekuntia,
+    // Viestin ajastusta voisi säätää enemmänkin,
+    // jotta joka viesti olisi aina sen 5 sekuntia,
     // mutta seuraavassa ainakin pieni ehto estämään päällekkäisiä ajastuksia
-    if (store.getState().notification === '') {
+    if (notification === "") {
       setTimeout(() => {
-        store.dispatch(clearMessage())
+        store.dispatch(clearMessage());
       }, 5000);
     }
     store.dispatch(vote(id, content));
@@ -18,12 +29,14 @@ const AnecdoteList = props => {
 
   return (
     <div>
-      {store.getState().anecdotes.map(anecdote => (
+      {anecdotesToShow().map(anecdote => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => upvote(anecdote.id, anecdote.content)}>vote</button>
+            <button onClick={() => upvote(anecdote.id, anecdote.content)}>
+              vote
+            </button>
           </div>
         </div>
       ))}
